901af4d62c49b3945dd1ceb4e747bf8ee2111508
Kimaya Joshi <kimayajoshi@Kimayas-MacBook-Air.local>
2020-11-26 21:36:29 +0530

    rewritten with hooks

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -13,7 +13,7 @@ class ErrorBoundary extends React.Component {
 	}
 
 	render() {
-		if (this.stats.hasError) {
+		if (this.state.hasError) {
 			return <h1> Oooops that is not good </h1>
 		}
 		return this.props.children
